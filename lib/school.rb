5d7117fd285827ab117d40515bc829af1b91737d
github username <github email address>
2021-05-18 17:58:25 -0400

    commit tree 290d3c6bc50f5dfefcf3012e0f0521b07deec7c9

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,60 @@
+require "pry"
+
+class School
+
+    attr_accessor :name, :roster
+
+def initialize(name)
+    @name = name
+    @roster = {}
+end
+
+def add_student(student_name, grade)
+    roster[grade] ||= []
+    roster[grade] << student_name
+end
+
+def grade(grade)
+    roster[grade]
+end
+
+def sort
+    new_array = {}
+    roster.each do |grade, students|
+        new_array[grade] = students.sort
+    end
+    new_array
+
+end
+
+end
+
 # code here!
+
+
+# class School
+#     attr_accessor :name, :roster
+  
+#     def initialize(name)
+#       @name = name
+#       @roster = {}
+#     end
+  
+#     def add_student(student_name, grade)
+#       roster[grade] ||= []
+#       roster[grade] << student_name
+#     end
+  
+#     def grade(student_grade)
+#       roster[student_grade]
+#     end
+  
+#     # this method should arrange the students in each grade by alphabetical order
+#     def sort
+#       sorted = {}
+#       roster.each do |grade, students|
+#         sorted[grade] = students.sort
+#       end
+#       sorted
+#     end
+#   end
